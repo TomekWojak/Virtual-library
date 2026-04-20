c3e7fae8433ba9dd5c769740e73a7f39aa4b2acc
Tomek <tomek.wojak796@gmail.com>
2026-04-21 00:12:57 +0200

    first commit

---
+++ dist/ts/script.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.querySelector(".mobile-btn");
+const mobileNavbar = document.querySelector(".mobile-navbar");
+const closeNavbarBtn = document.querySelector(".hide-navbar-btn");
+const handleMobileNavBar = () => {
+    mobileNavbar?.classList.toggle("delay-300");
+    mobileNavbar?.classList.toggle("translate-x-full");
+    const lineTop = burgerBtn?.querySelector(".line-top");
+    const lineBottom = burgerBtn?.querySelector(".line-bottom");
+    lineTop?.classList.toggle("w-full");
+    lineBottom?.classList.toggle("w-full");
+};
+burgerBtn?.addEventListener("click", handleMobileNavBar);
+closeNavbarBtn?.addEventListener("click", handleMobileNavBar);
+export {};
---
+++ src/ts/script.ts
@@ -0,0 +1,17 @@
+const burgerBtn = document.querySelector<HTMLElement>(".mobile-btn");
+const mobileNavbar = document.querySelector<HTMLElement>(".mobile-navbar");
+const closeNavbarBtn = document.querySelector(".hide-navbar-btn");
+
+const handleMobileNavBar = () => {
+	mobileNavbar?.classList.toggle("delay-300");
+	mobileNavbar?.classList.toggle("translate-x-full");
+
+	const lineTop = burgerBtn?.querySelector(".line-top");
+	const lineBottom = burgerBtn?.querySelector(".line-bottom");
+
+	lineTop?.classList.toggle("w-full");
+	lineBottom?.classList.toggle("w-full");
+};
+
+burgerBtn?.addEventListener("click", handleMobileNavBar);
+closeNavbarBtn?.addEventListener("click", handleMobileNavBar);
